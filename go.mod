e104acc55f14c756b17583d75bb87912c1aca055
samjove <vermo7en@gmail.com>
2024-09-28 00:10:17 -0400

    Added html lib and node traversal function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/samjove/link-parser
 
 go 1.23.1
+
+require golang.org/x/net v0.29.0 // indirect
--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,11 @@
 package parser
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Represents a HTML link
 type Link struct {
@@ -10,5 +15,21 @@ type Link struct {
 
 // Takes an HTML doc and returns a slice of Links parsed from it
 func Parse(r io.Reader) ([]Link, error) {
-	return nil, nil
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	traverse(doc, "")
+	return nil, nil 
+}
+
+func traverse(n *html.Node, padding string) {
+	data := n.Data
+	if n.Type == html.ElementNode {
+		data = "<" + data + ">"
+	}
+	fmt.Println(padding, data)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		traverse(c, padding + "  ")
+	}
 }
